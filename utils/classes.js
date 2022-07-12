9addf4fd77b14cfbc24c76fbaa68ae643e79b9af
Balamantegada <rodrigo.kojack@gmail.com>
2022-07-12 11:38:53 -0300

    layout

---
+++ utils/classes.js
@@ -0,0 +1,27 @@
+const classes = {
+  main: {
+    marginTop: 2,
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 1,
+    textAlign: 'center',
+    minHeight: '10vh',
+  },
+  appbar: {
+    backgroundColor: '#131212',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 1,
+    },
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '2.5rem',
+  },
+};
+
+export default classes;
